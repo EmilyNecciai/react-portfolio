91d93def177abed8efc4915d8528c57c7d72f00f
Emily N Mayeski <enmayeski@gmail.com>
2021-10-06 18:57:27 -0400

    css work

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,9 +5,9 @@ import React from "react";
 function Project(props) {
   return (
     <div>
-      <div className="columns is-desktop">
+      <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
         {props.projects.map((project) => (
-          <div className="column is-one-third">
+          <div className="column is-half">
             <div className="card">
               <div className="card-image">
                 <figure className="image is-4by3">
